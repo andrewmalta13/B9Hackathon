8b8a4d12db6852cbfe852851d4eae49411c92fd9
Andrew Malta <andrew.malta@yale.edu>
2015-01-09 10:16:22 -0500

    course object structure and json output

--- 9book/main.py
+++ 9book/main.py
@@ -1,22 +1,32 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
+import json
+from google.appengine.ext import ndb
 
 class MainHandler(webapp2.RequestHandler):
+    
+
+    #ndb model class for each Course that we are storing.
+    class Course(ndb.Model):
+      title = ndb.StringProperty()
+      professor = ndb.StringProperty()
+      time = ndb.StringProperty()
+      location = ndb.StringProperty()
+      distReqAreas = ndb.StringProperty()
+      term = ndb.StringProperty()
+
+      description = ndb.TextProperty()
+
+      permissionRequired = ndb.BooleanProperty()
+      readingPeriod = ndb.BooleanProperty()
+
+      classRating = ndb.floatProperty() 
+      professorRating = ndb.floatProperty()
+      workRating = ndb.floatProperty()
+
+      courseNum = ndb.IntegerProperty()
+      
+
+
     def get(self):
         self.response.write('Hello world!')
 
