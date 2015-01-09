e4224d23d6caec3cd555ac16dd7e839c488e3b79
Andrew Malta <andrew.malta@yale.edu>
2015-01-09 12:44:37 -0500

    changed file structures

--- 9book/main.py
+++ 9book/main.py
@@ -2,6 +2,27 @@ import webapp2
 import json
 import coursestats
 
+from google.appengine.ext import ndb
+
+#ndb model class for each Course that we are storing.
+class Course(ndb.Model):
+    title = ndb.StringProperty()
+    professor = ndb.StringProperty()
+    time = ndb.StringProperty()
+    location = ndb.StringProperty()
+    distReqAreas = ndb.StringProperty()
+    term = ndb.StringProperty()
+
+    description = ndb.TextProperty()
+
+    permissionRequired = ndb.BooleanProperty()
+    readingPeriod = ndb.BooleanProperty()
+
+    classRating = ndb.floatProperty() 
+    professorRating = ndb.floatProperty()
+    workRating = ndb.floatProperty()
+
+    courseNum = ndb.IntegerProperty()
 
 class JSONHandler(webapp2.RequestHandler):
     def get(self):
