601542efe47e1b4cf6ea58cf91b2ede7930be7b4
Andrew Malta <andrew.malta@yale.edu>
2015-01-10 13:52:01 -0500

    now can fetch all courses from oci, added a text file of the json for all of the courses.

--- 9book/main.py
+++ 9book/main.py
@@ -25,6 +25,8 @@ class Course(ndb.Model):
     professorRating = ndb.FloatProperty()
     workRating = ndb.FloatProperty()
 
+    OCInumber = ndb.IntegerProperty()
+
     
 
 class JSONHandler(webapp2.RequestHandler):
@@ -32,11 +34,11 @@ class JSONHandler(webapp2.RequestHandler):
         courses = ndb.gql("SELECT * FROM Course")
         coursesjson = {"courses": []}
         for course in courses:
-            coursesjson.append({"title": course.title,
+            coursesjson["courses"].append({"title": course.title,
                                 "professor": course.professor,
                                 "time": course.time,
                                 "location": course.location,
-                                "distReqAreas": course.disReqAreas,
+                                "distReqAreas": course.distReqAreas,
                                 "term": course.term,
                                 "description": course.description,
                                 "instructorPermissionRequired": course.instructorPermissionRequired,
@@ -45,7 +47,8 @@ class JSONHandler(webapp2.RequestHandler):
                                 "classRating": course.classRating,
                                 "professorRating": course.professorRating,
                                 "workRating": course.workRating,
-                                "courseNum": course.courseNum})
+                                "courseNum": course.courseNum,
+                                "OCInumber": course.OCInumber})
 
         self.response.write(json.dumps(coursesjson))
 
@@ -53,6 +56,7 @@ class JSONHandler(webapp2.RequestHandler):
 
 class FetchCoursesHandler(webapp2.RequestHandler):
     def get(self):
+
         for i in range (20001, 30000):
           courseText = OCIScraper.courseNumberTest(i)
           if courseText:
@@ -70,8 +74,11 @@ class FetchCoursesHandler(webapp2.RequestHandler):
                        classRating = courseInfoDict["classRating"],
                        professorRating = courseInfoDict["professorRating"],
                        workRating = courseInfoDict["workRating"],
-                       courseNum = courseInfoDict["courseNum"])
+                       courseNum = courseInfoDict["courseNum"],
+                       OCInumber = i)
             c.put()
+
+
 
 
 app = webapp2.WSGIApplication([
